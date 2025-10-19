7d7a2ff39644278140f21956ceaf0a3e12abf597
JP <jp.greasefiregames@gmail.com>
2025-10-19 13:53:17 -0400

    completed build and submitted

--- scripts/handlers/GenerationHandler.gd
+++ scripts/handlers/GenerationHandler.gd
@@ -16,13 +16,15 @@ var inputOption : PackedScene = preload("res://assets/scenes/input_option.tscn")
 
 var inputOptionArray : Array[InputOption] = []
 
+
 @export_category("TileMap Settings")
 @export var tileMap : TileMapLayer
+var game_map : GameMap
 
 var floor_atlas := Vector2(0, 0)
 var wall_atlas := Vector2(1, 0)
 
-#TODO implement x to toggle UI
+
 func _unhandled_key_input(event):
 	if event.is_pressed() and event.keycode == KEY_X:
 		if self.visible == true: self.visible = false
@@ -38,41 +40,38 @@ func _on_d_generator_button_item_selected(index):
 
 func generate():
 	var _dungeonGenerator
-	var _game_map : GameMap
-	_game_map = GameMap.new(int(i_MAPWIDTH_FIELD.text), int(i_MAPHEIGHT_FIELD.text))
+	game_map = GameMap.new(int(i_MAPWIDTH_FIELD.text), int(i_MAPHEIGHT_FIELD.text))
 	match generatorChoice:
 		dungeonType.RECT_CORRIDOR:
 			var _max_rooms = int(inputOptionArray[0].inputField.text)
 			var _min_rooms = int(inputOptionArray[1].inputField.text)
 			var _max_room_w = int(inputOptionArray[2].inputField.text)
 			var _max_room_h = int(inputOptionArray[3].inputField.text)
-			_dungeonGenerator = RectangleDungeonGenerator.new(_max_rooms, _min_rooms, _max_room_h, _max_room_w, _game_map)
+			_dungeonGenerator = RectangleDungeonGenerator.new(_max_rooms, _min_rooms, _max_room_h, _max_room_w, game_map)
 			_dungeonGenerator.genDungeon()
-			bakeMap(_game_map)
+			bakeMap(game_map)
 		dungeonType.DRUNKEN_WALK:
 			var _walkers = int(inputOptionArray[0].inputField.text)
 			var _steps = int(inputOptionArray[1].inputField.text)
-			_dungeonGenerator = DrunkWalkDungeonGenerator.new(_game_map, _steps, _walkers)
+			_dungeonGenerator = DrunkWalkDungeonGenerator.new(game_map, _steps, _walkers)
 			_dungeonGenerator.genDungeon()
-			bakeMap(_game_map)
+			bakeMap(game_map)
 		dungeonType.PERLIN_NOISE:
 			var _frequency : float = float(inputOptionArray[0].inputField.text)
 			var _fractal_octaves : int = int(inputOptionArray[1].inputField.text)
 			var _fractal_lacunarity : float = float(inputOptionArray[2].inputField.text)
 			var _fractal_gain : float = float(inputOptionArray[3].inputField.text)
-			
-			_dungeonGenerator = PerlinNoiseDungeonGenerator.new(_game_map, randi(), _frequency, _fractal_octaves, _fractal_lacunarity, _fractal_gain)
+			_dungeonGenerator = PerlinNoiseDungeonGenerator.new(game_map, randi(), _frequency, _fractal_octaves, _fractal_lacunarity, _fractal_gain)
 			_dungeonGenerator.genDungeon()
-			bakeMap(_game_map)
+			bakeMap(game_map)
 		dungeonType.CELLULAR_AUTOMATA:
 			var placement: float = float(inputOptionArray[0].inputField.text)
 			var survival: int = int(inputOptionArray[1].inputField.text)
 			var birth: int = int(inputOptionArray[2].inputField.text)
 			var generations: int = int(inputOptionArray[2].inputField.text)
-			
-			_dungeonGenerator = CellularAutomataDungeonGenerator.new(_game_map, placement, survival, birth, generations)
+			_dungeonGenerator = CellularAutomataDungeonGenerator.new(game_map, placement, survival, birth, generations)
 			_dungeonGenerator.genDungeon()
-			bakeMap(_game_map)
+			bakeMap(game_map)
 
 func setupGenUI(type:dungeonType):
 	match type:
@@ -150,7 +149,7 @@ func showWarning(warningText: String):
 	await get_tree().create_timer(2).timeout
 	warningPanel.visible = false
 
-func bakeMap(game_map: GameMap) -> void:
+func bakeMap(_game_map: GameMap) -> void:
 	if tileMap == null:
 		push_error("No Tile Map added to export field.")
 		return
@@ -159,8 +158,8 @@ func bakeMap(game_map: GameMap) -> void:
 	@warning_ignore("unused_variable")
 	var tileSet := tileMap.tile_set
 	var source_id = tileMap.tile_set.get_source_id(0)
-	for y in range(game_map.map_h):
-		for x in range(game_map.map_w):
-			var cell = game_map.map[x][y]
+	for y in range(_game_map.map_h):
+		for x in range(_game_map.map_w):
+			var cell = _game_map.map[x][y]
 			var atlas := wall_atlas if (cell is WallCell) else floor_atlas
 			tileMap.set_cell(Vector2i(x, y), source_id, atlas)
---
+++ scripts/pathing/PathFinder.gd
@@ -0,0 +1,121 @@
+class_name PathFinder
+extends Node2D
+
+@export var tileMap : TileMapLayer
+var game_map: GameMap
+var start_point : Vector2i
+var end_point : Vector2i
+var enabled : bool = true
+
+var path: Array[Vector2i] = []
+# tileMap.local_to_map(tileMap.get_local_mouse_position()) Returns the local tile location in the grid.
+
+func _init(_game_map : GameMap, _tile_map : TileMapLayer):
+	game_map = _game_map
+	tileMap = _tile_map
+	start_point = Vector2.ZERO
+	end_point = Vector2.ZERO
+	# enabled = false # (for later)
+
+func clearPoints():
+	start_point = Vector2i.ZERO
+	end_point = Vector2i.ZERO
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and enabled:
+		if start_point == Vector2i.ZERO && end_point == Vector2i.ZERO:
+			start_point = tileMap.local_to_map(tileMap.get_local_mouse_position())
+		elif start_point != Vector2i.ZERO && end_point == Vector2i.ZERO:
+			end_point = tileMap.local_to_map(tileMap.get_local_mouse_position())
+			path = djikstra_pathfind(start_point, end_point)
+			queue_redraw()
+		elif (start_point != Vector2i.ZERO && end_point != Vector2i.ZERO):
+			clearPoints()
+			clear_path()
+		
+		return
+
+
+func show_path(new_path: Array[Vector2i]) -> void:
+	path = new_path.duplicate(true)
+	queue_redraw()
+
+func clear_path() -> void:
+	path.clear()
+	queue_redraw()
+
+func _draw() -> void:
+	if path.is_empty() or tileMap == null:
+		return
+	
+	var tile_size: Vector2 = Vector2(tileMap.tile_set.tile_size)
+	
+	for cell in path:
+		var local_pos: Vector2 = tileMap.map_to_local(cell)
+		var rect := Rect2(local_pos - tile_size * 0.5, tile_size)
+		
+		draw_rect(rect, Color(0.2, 0.6, 1.0, 0.4), true)
+
+func get_walkable_neighbors(root: Vector2i) -> Array:
+	var neighbors = []
+
+	for i in range(0, 4):
+		match i:
+			0:
+				var n = game_map.map[root.x][root.y - 1] as MapCell
+				if n.walkable: neighbors.append(n)
+			1:
+				var n = game_map.map[root.x][root.y + 1] as MapCell
+				if n.walkable: neighbors.append(n)
+			2:
+				var n = game_map.map[root.x + 1][root.y] as MapCell
+				if n.walkable: neighbors.append(n)
+			3:
+				var n = game_map.map[root.x - 1][root.y] as MapCell
+				if n.walkable: neighbors.append(n)
+	return neighbors
+
+
+func djikstra_pathfind(_start_point : Vector2i, _end_point : Vector2i) -> Array:
+	var frontier : Array[Vector2i] = [] # nodes to explore next
+	var came_from : Dictionary = {}
+	var cost_so_far : Dictionary = {}
+	
+	frontier.append(_start_point)
+	came_from[_start_point] = null
+	cost_so_far[_start_point] = 0
+	
+	while frontier.size() > 0:
+		# pick node with lowest cost
+		@warning_ignore("confusable_local_declaration")
+		var current = frontier[0]
+		for node in frontier:
+			if cost_so_far[node] < cost_so_far[current]:
+				current = node
+	
+		frontier.erase(current)
+		
+		if current == _end_point:
+			break
+		
+		for neighbor_cell in get_walkable_neighbors(current):
+			var neighbor_pos = neighbor_cell.position
+			var new_cost = cost_so_far[current] + 1
+			
+			if !cost_so_far.has(neighbor_pos) or new_cost < cost_so_far[neighbor_pos]:
+				cost_so_far[neighbor_pos] = new_cost
+				came_from[neighbor_pos] = current
+				frontier.append(neighbor_pos)
+				
+	@warning_ignore("shadowed_variable")
+	var path : Array[Vector2i] = []
+	if not came_from.has(_end_point):
+		print("No available Path.")
+		return []
+	
+	var current = _end_point
+	while current != null:
+		path.insert(0, current)
+		current = came_from.get(current, null)
+	
+	return path
